ca14db3b8d9317dadea954c18e19f09ed0e7c660
Carlos <rocarlos1205@gmail.com>
2024-05-16 05:21:02 -0500

    first commit

---
+++ src/app/components/HelloWorld.jsx
@@ -0,0 +1,5 @@
+export function HelloWorld(){
+    return(
+        <h1>Hola Mundo!</h1>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import {HelloWorld} from "@/app/components/HelloWorld"
+
+export default function Home() {
+  return (
+    <HelloWorld/>
+  )
+}
