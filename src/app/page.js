6bb1c765e6b92634b2c86c24ebdd2e31ce1a14f0
Carlos <rocarlos1205@gmail.com>
2024-05-19 14:15:59 -0500

    Add Main Page Styles
    Metadata Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,31 @@
-import {HelloWorld} from "@/app/components/HelloWorld"
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <HelloWorld/>
+    <>
+    <header>
+
+      <section>
+        <span>
+          <Image src="/logo.png" alt="Hola" width={100} height={100} className="m-6"/>
+        </span>
+        <h1 className="ml-6 text-4xl w-fit">DERECHOS <br/> HUMANOS</h1>
+        <h1 className="mt-1 text-2xl text-center blue-gelco">¡Comencemos la formación!</h1>
+        <a href="/" className="flex justify-center mt-2 text-xl font-bold pink-gelco">Comenzar</a>
+
+        <div className="absolute top-0 right-0 m-6">
+          <button><Image src="/uk-icon.png" alt="UK" width={30} height={30} /></button>
+          <button><Image src="/spain-icon.png" alt="UK" width={30} height={30} /></button>
+        </div>
+      </section>
+      <section>
+        
+      </section>
+    </header>
+
+    <main>
+    </main>
+
+    </>
   )
 }
