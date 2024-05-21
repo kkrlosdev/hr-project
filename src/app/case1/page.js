74ef625115a7034171297117515239d480facc76
Carlos <rocarlos1205@gmail.com>
2024-05-21 15:57:10 -0500

    Add Favicon to Head and Update Head Title
    Case 1 Page Updated

--- src/app/case1/page.js
+++ src/app/case1/page.js
@@ -20,15 +20,43 @@ export default function Case1(){
                 </section>
                 
             </header>
-            <hr></hr>
-            <section className="flex flex-col justify-center mx-auto mb-4" style={{width: "650px"}}>
-                <h1 className="my-6 text-2xl font-semibold cyan-gelco">Introducción</h1>
-                <p className="text-xl text-justify">
+
+            <hr className="hr-gradient"/>
+
+            <section className="flex flex-col items-center justify-center mx-auto mb-4 text-justify" style={{width: "650px"}}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="my-4 w-14 h-14">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                </svg>
+
+                <h1 className="self-start my-3 text-2xl font-semibold cyan-gelco">Introducción</h1>
+                <p className="self-start text-xl">
                     ¿Imaginas algo <span className="cyan-gelco">básico, innato y universal?</span>
                 </p>
-                <p className="text-xl text-justify">
+
+                <p className="text-xl">
                     Algo <span className="cyan-gelco">lo suficientemente importante para implicar </span>a 193 países, traducirse a más de 300 idiomas e involucrar a más de 7 mil millones de personas.
                 </p>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="my-4 w-14 h-14">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                </svg>
+
+                <p className="text-xl">
+                    Algo que te <span className="cyan-gelco">afecta no solo a ti,</span> sino también a todas las personas que conoces y quieres... ¡y a todas aquellas que ni siquiera conoces!
+                </p>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="my-4 w-14 h-14">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                </svg>
+
+                <p className="text-xl">
+                    Algo que implica que podamos desarrollar una <span className="cyan-gelco">vida en común digna, satisfactoria e igualitaria,</span> ¿lo imaginas?
+                </p>
+
+                <hr className="hr-gradient"/>
+
+                <h1 className="self-start my-2 text-2xl font-semibold cyan-gelco">¿Qué son los derechos humanos?</h1>
+
             </section>
         </>
     )
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,18 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Recursos Humanos Gelco",
+  title: "DDHH Gelco",
   description: "Formación sobre derechos humanos para Gelco",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/gorro-de-graduacion.svg"/>
       <body className={inter.className}>{children}</body>
     </html>
   );
