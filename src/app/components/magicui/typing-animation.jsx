4168185fc9c7d2416b450a450d933d83395288c8
Carlos <rocarlos1205@gmail.com>
2024-05-25 21:55:06 -0500

    Add Typing Animation Component
    Typing Animation and Gradient in Main Page

---
+++ src/app/components/magicui/typing-animation.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useState } from "react";
+import { cn } from "@/app/lib/utils";
+
+export default function TypingAnimation({ text, duration = 200, className }) {
+  const [displayedText, setDisplayedText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const typingEffect = setInterval(() => {
+      setDisplayedText((prev) => prev + text.charAt(index));
+      setIndex((prev) => prev + 1);
+    }, duration);
+
+    if (index === text.length) {
+      clearInterval(typingEffect);
+    }
+
+    return () => clearInterval(typingEffect);
+  }, [index, text, duration]);
+
+  return (
+    <h1
+      className={cn(
+        "text-center font-display text-2xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-4xl md:leading-[5rem]",
+        className
+      )}
+    >
+      {displayedText}
+    </h1>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import {Logo} from "./components/Logo"
 import UkIcon from "@/app/assets/uk-icon.png"
 import EsIcon from "@/app/assets/spain-icon.png"
+import TypingAnimation from "./components/magicui/typing-animation";
 
 export default function Home() {
   return (
@@ -11,18 +12,18 @@ export default function Home() {
       <section>
         <Logo />
         <h1 className="ml-6 text-4xl w-fit blue-gelco">DERECHOS <br/> HUMANOS</h1>
-        <h1 className="mt-1 text-2xl text-center blue-gelco">¡Comencemos la formación!</h1>
-        <div className="flex justify-center mt-2">
-          <a href="/cases" className="mt-2 text-xl font-bold pink-gelco">Comenzar</a>
+        <div style={{ minHeight: '120px' }} className="flex flex-col justify-between">
+          <TypingAnimation text="¡Comencemos la formación!" duration={65} className="text-gradient"/>
+          <div className="flex justify-center mt-1">
+            <a href="/cases" className="text-2xl font-bold pink-gelco">Comenzar</a>
+          </div>
         </div>
 
         <div className="absolute top-0 right-0 m-6">
           <button className="mx-1"><Image src={UkIcon} alt="UK" width={30} height={30} /></button>
           <button><Image src={EsIcon} alt="UK" width={30} height={30} /></button>
         </div>
-      </section>
-      <section>
-        
+
       </section>
     </header>
 
