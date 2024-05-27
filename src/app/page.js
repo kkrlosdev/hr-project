657802562942ff7563ef8642a4eed9ec411d0e6f
Carlos <rocarlos1205@gmail.com>
2024-05-26 20:37:11 -0500

    Add Background Video to Main Page

--- src/app/page.js
+++ src/app/page.js
@@ -9,15 +9,19 @@ export default function Home() {
     <>
     <header>
 
-      <section>
+      <section className="SectionWelcomePage">
         <Logo />
-        <h1 className="ml-6 text-4xl w-fit blue-gelco">DERECHOS <br/> HUMANOS</h1>
+        <h1 className="ml-6 text-4xl font-semibold text-gradient w-fit">DERECHOS <br/> HUMANOS</h1>
         <div style={{ minHeight: '120px' }} className="flex flex-col justify-between">
           <TypingAnimation text="¡Comencemos la formación!" duration={65} className="text-gradient"/>
           <div className="flex justify-center mt-1">
             <a href="/cases" className="text-2xl font-bold pink-gelco">Comenzar</a>
           </div>
         </div>
+
+        <video muted autoPlay loop className="absolute top-0 left-0 object-cover w-full h-full -z-10">
+            <source src="/videos/background-video.mp4" type="video/mp4"></source>
+        </video>
 
         <div className="absolute top-0 right-0 m-6">
           <button className="mx-1"><Image src={UkIcon} alt="UK" width={30} height={30} /></button>
@@ -26,10 +30,6 @@ export default function Home() {
 
       </section>
     </header>
-
-    <main>
-    </main>
-
     </>
   )
 }
