2852d49b236f194b21bb985a6a8bba00cccf9aaa
Carlos <rocarlos1205@gmail.com>
2024-05-28 12:59:25 -0500

    Replace <a> by <Link>

--- src/app/components/BackButton.jsx
+++ src/app/components/BackButton.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link"
+
 export function BackButton(){
     return(
-        <a href="/cases" className="fixed z-50 flex items-center justify-center w-24 h-12 gap-1 text-white transition-all rounded-md shadow-md top-4 left-4 bg-cyan-gelco hover:shadow-lg hover:bg-cyan-600"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <Link href="/cases" className="fixed z-50 flex items-center justify-center w-24 h-12 gap-1 text-white transition-all rounded-md shadow-md top-4 left-4 bg-cyan-gelco hover:shadow-lg hover:bg-cyan-600"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                     </svg>
-                Volver</a>
+                Volver</Link>
     )
 }
--- src/app/components/KnowMore.jsx
+++ src/app/components/KnowMore.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export function KnowMoreButton({color, href}){
   const bgColorClass = `${color}`;
   return (
-      <a href={href} onClick={(e)=> e.stopPropagation()} className={`flex ${bgColorClass} items-center justify-center w-64 h-12 gap-3 mt-2 text-white rounded hover:shadow-md`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <Link href={href} onClick={(e)=> e.stopPropagation()} className={`flex ${bgColorClass} items-center justify-center w-64 h-12 gap-3 mt-2 text-white rounded hover:shadow-md`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
     </svg>    
-    ¿Quieres saber más?</a>
+    ¿Quieres saber más?</Link>
   )
 }
