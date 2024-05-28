0ff3d719120b56b299ba1043fa1f1e9c11827d36
Carlos <rocarlos1205@gmail.com>
2024-05-28 13:52:36 -0500

    Disable Inputs if Selected

--- src/app/case1/components/Activity1.jsx
+++ src/app/case1/components/Activity1.jsx
@@ -16,11 +16,11 @@ export function Activity1(){
                 <p className="">Los derechos humanos se clasifican en 2 categorías</p>
                 <div className="w-full text-white">
                     <div className="p-3 my-2 rounded bg-cyan-gelco">
-                        <input type="radio" value="value1" id="opcion1" checked={selectedValue === 'value1'} onChange={handleChange}/>
+                        <input type="radio" value="value1" id="opcion1" checked={selectedValue === 'value1'} onChange={handleChange} disabled={selectedValue !== ''}/>
                         <label htmlFor="opcion1" className="ml-2">Verdadero</label>
                     </div>
                     <div className="p-3 my-2 rounded bg-cyan-gelco">
-                        <input type="radio" value="value2" id="opcion2" checked={selectedValue === 'value2'} onChange={handleChange}/>
+                        <input type="radio" value="value2" id="opcion2" checked={selectedValue === 'value2'} onChange={handleChange} disabled={selectedValue !== ''}/>
                         <label htmlFor="opcion2" className="ml-2">Falso</label>
                     </div>
                 <hr className="my-4"/>
@@ -32,7 +32,7 @@ export function Activity1(){
                     </>
                     
                 ) : selectedValue ==='value1' ? (
-                    <div className="p-1 text-center text-white bg-red-600 rounded">Incorrecto, intenta de nuevo.</div>
+                    <div className="p-1 text-center text-white bg-red-600 rounded">Incorrecto, se clasifican en 3 categorías.</div>
                 ) : (
                     <div></div>
                 )}
