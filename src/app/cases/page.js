7b374c30fe4efa68981828438e6c51140d20a3a2
Carlos <rocarlos1205@gmail.com>
2024-06-04 13:17:12 -0500

    Add Background Video to Cases Page
    Update InfoCase and KnowMore Components

--- src/app/cases/page.js
+++ src/app/cases/page.js
@@ -14,6 +14,10 @@ export default function Cases(){
             <header>
                 <Logo />
                 <h1 className="ml-6 text-3xl blue-gelco">DERECHOS <br/> HUMANOS </h1>
+
+                <video muted autoPlay loop className="absolute top-0 left-0 object-cover w-full h-full -z-10">
+                    <source src="/videos/background-video2.mp4" type="video/mp4"></source>
+                </video>
             </header>
 
             <main className="flex justify-center my-2 space-x-36">
@@ -48,7 +52,7 @@ export default function Cases(){
                     <Link href="/case3">
                         <div className="my-3 text-xl font-semibold text-white transition-all duration-500 rounded w-72 h-72 bg-cyan-gelco hover:scale-105">
                             <h1 className="m-3">Remediación</h1>
-                            <Image className="mt-5 ml-6" src={remediacion_card} alt="Derechos humanos" width={250} height={100}/>
+                            <Image className="mt-5 ml-6" src={remediacion_card} alt="Remediación" width={250} height={100}/>
                         </div>
                     </Link>
                     <KnowMoreButton color="bg-cyan-gelco" href="./case3/resources"/>
--- src/app/components/InfoCase.jsx
+++ src/app/components/InfoCase.jsx
@@ -4,12 +4,12 @@ export function InfoCase({subtitle}){
     return(
         <section style={{width: "50%"}} className="relative top-36 left-1">
 
-                    <div className="flex flex-col items-center justify-center h-56 space-y-6 text-white bg-blue-gelco">
-                        <h1 className="text-6xl">Derechos Humanos</h1>
+                    <div className="flex flex-col items-center justify-center space-y-6 text-white rounded-3xl h-72 bg-blue-gelco">
+                        <h1 className="text-5xl">Derechos Humanos</h1>
                         <h2 className="text-2xl">{subtitle}</h2>
-                    </div>
-                    <div className="flex items-center justify-center h-16 bg-white p-7">
-                        <Logo/>
+                        <div className="flex items-center justify-center w-full h-16 bg-white p-7">
+                            <Logo/>
+                        </div>
                     </div>
 
         </section>
