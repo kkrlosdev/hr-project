4d8a10bf87f90edf493457a79e8f35978de919bf
Carlos <rocarlos1205@gmail.com>
2024-06-05 16:48:12 -0500

    style: add a non-important change to activity compononets

--- src/app/case3/components/activities/Activity2.jsx
+++ src/app/case3/components/activities/Activity2.jsx
@@ -1,7 +1,9 @@
 export function Activity2(){
     return(
         <>
-            <h1>Actividad 2</h1>
+            <div className="w-full p-4 mt-5 border border-black rounded-md">
+                <h1>Hola doMun</h1>
+            </div>
         </>
     )
 }
