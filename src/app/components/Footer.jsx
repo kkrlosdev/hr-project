e597587db67f3822914a4a03d025b495398c1fb9
Carlos <rocarlos1205@gmail.com>
2024-06-08 11:53:48 -0500

    Add React-Spinners
    Remove Github Icon from Footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,8 @@
-import Github from "./GitHub"
-import Link from "next/link"
-
 export function Footer(){
     return(
         <>
             <div className="flex items-center justify-center w-full h-10 text-white bg-blue-gelco">
-                2024.
-                <Link href="https://github.com/kkrlosdev/hr-project" target="_blank" className="ml-3">
-                    <Github/>
-                </Link>
+                &copy; 2024.
             </div>
         </>
     
