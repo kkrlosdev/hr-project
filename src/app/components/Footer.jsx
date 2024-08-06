e5ca68fb1431612510bbb10c22e34e7dded39e8e
Carlos <rocarlos1205@gmail.com>
2024-08-06 16:47:52 -0500

    add footer, fix header

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,44 @@
+import Link from "next/link"
+import Image from "next/image"
+import LogoBlanco from "@/app/assets/logo-blanco.png"
+
 export function Footer(){
     return(
         <>
-            <div className="flex items-center justify-center w-full h-10 text-white bg-blue-gelco">
-                &copy; 2024.
+            <div className="flex items-center justify-between w-full h-full mt-10 text-white bg-blue-gelco">
+                <div className="flex flex-col items-center ml-[170px]">
+                    <div className="my-6">
+                        <Image src={LogoBlanco} height={130} width={130} alt="Logo Gelco"/>
+                    </div>
+                    <p className="ml-6">¡Construyendo la Gelco del futuro!</p>
+                </div>
+                <div className="flex items-center mr-16 justify-items-center gap-x-10">
+                    <div className="my-10">
+                        <p className="mb-1 text-xl font-semibold">Recursos</p>
+                        <ul className="flex flex-col gap-y-2">
+                            <Link className="text-glow hover:underline" href="/">Conferencia anual</Link>
+                            <Link className="text-glow hover:underline" href="/">Infografía</Link>
+                            <Link className="text-glow hover:underline" href="/">Vídeo</Link>
+                        </ul>
+                    </div>
+                    <div className="my-10">
+                        <h2 className="mb-1 text-xl font-semibold">Sobre nosotros</h2>
+                        <ul className="flex flex-col gap-y-2">
+                            <Link className="text-glow hover:underline" href="/">Contacto</Link>
+                            <Link className="text-glow hover:underline" href="/">Manual de convivencia</Link>
+                            <Link className="text-glow hover:underline" href="/">Lineamientos Gelco</Link>
+                        </ul>
+                    </div>
+                    <div className="my-10">
+                        <h2 className="mb-1 text-xl font-semibold">Legal</h2>
+                        <ul className="flex flex-col gap-y-2">
+                            <Link className="text-glow hover:underline" href="/">Términos y condiciones</Link>
+                            <Link className="text-glow hover:underline" href="/">Políticas de privacidad</Link>
+                            <Link className="text-glow hover:underline" href="/">Proveedores</Link>                        
+                        </ul>
+                    </div>
+                </div>
             </div>
         </>
-    
     )
 }
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from '@/app/assets/logo.png';
 
 export function Header() {
     return (
-        <header className="fixed z-20 flex items-center h-16 shadow-xl bg-stone-100" style={{width:"99.29%"}}>
+        <header className="fixed z-20 flex items-center h-16 shadow-xl bg-stone-100" style={{width:"100%"}}>
             <div className="flex justify-center w-full">
                 <Image src={Logo} alt="Logo" className="transition-all hover:-rotate-3" />
             </div>
