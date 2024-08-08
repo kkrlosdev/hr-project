1508201d4b903ad764627e0c8db342b7bbc59bfd
Carlos <rocarlos1205@gmail.com>
2024-08-07 22:22:11 -0500

    tweaks: update font color in footer and gap

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,36 +5,36 @@ import LogoBlanco from "@/app/assets/logo-blanco.png"
 export function Footer(){
     return(
         <>
-            <footer className="flex items-center justify-between w-full h-full mt-10 text-slate-300 bg-blue-gelco">
+            <footer className="flex items-center justify-between w-full h-full mt-10 text-slate-400 bg-blue-gelco">
                 <div className="flex flex-col items-center ml-[170px] my-8">
                     <div className="my-6 ml-4">
                         <Image src={LogoBlanco} height={130} width={130} alt="Logo Gelco"/>
                     </div>
-                    <p className="ml-6 text-white">¡Construyendo la Gelco del futuro!</p>
+                    <p className="ml-6">¡Construyendo la Gelco del futuro!</p>
                 </div>
                 <div className="flex items-center my-8 mr-20 justify-items-center gap-x-16">
                     <div className="my-10">
                         <p className="my-1 text-xl font-semibold text-white">Recursos</p>
-                        <ul className="flex flex-col gap-y-2">
-                            <Link className="text-glow hover:underline" href="#">Conferencia anual</Link>
-                            <Link className="text-glow hover:underline" href="#">Infografía</Link>
-                            <Link className="text-glow hover:underline" href="#">Vídeo</Link>
+                        <ul className="flex flex-col gap-y-3">
+                            <Link className="hover:text-white hover:underline" href="#">Conferencia anual</Link>
+                            <Link className="hover:text-white hover:underline" href="#">Infografía</Link>
+                            <Link className="hover:text-white hover:underline" href="#">Vídeo</Link>
                         </ul>
                     </div>
                     <div className="my-10">
                         <h2 className="my-1 text-xl font-semibold text-white">Sobre nosotros</h2>
-                        <ul className="flex flex-col gap-y-2">
-                            <Link className="text-glow hover:underline" href="#">Contacto</Link>
-                            <Link className="text-glow hover:underline" href="#">Manual de convivencia</Link>
-                            <Link className="text-glow hover:underline" href="#">Lineamientos Gelco</Link>
+                        <ul className="flex flex-col gap-y-3">
+                            <Link className="hover:text-white hover:underline" href="#">Contacto</Link>
+                            <Link className="hover:text-white hover:underline" href="#">Manual de convivencia</Link>
+                            <Link className="hover:text-white hover:underline" href="#">Lineamientos Gelco</Link>
                         </ul>
                     </div>
                     <div className="my-10">
                         <h2 className="my-1 text-xl font-semibold text-white">Legal</h2>
-                        <ul className="flex flex-col gap-y-2">
-                            <Link className="text-glow hover:underline" href="#">Términos y condiciones</Link>
-                            <Link className="text-glow hover:underline" href="#">Políticas de privacidad</Link>
-                            <Link className="text-glow hover:underline" href="#">Proveedores</Link>                        
+                        <ul className="flex flex-col gap-y-3">
+                            <Link className="hover:text-white hover:underline" href="#">Términos y condiciones</Link>
+                            <Link className="hover:text-white hover:underline" href="#">Políticas de privacidad</Link>
+                            <Link className="hover:text-white hover:underline" href="#">Proveedores</Link>                        
                         </ul>
                     </div>
                 </div>
